168a4d3e532c38c52fc6c45e8a8f3dc6e757d6c6
mykola <mykola@emil.de>
2022-11-20 14:07:26 +0100

    chore(client): clean up

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from "react-dom/client";
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './App/hooks/useAuth';
 import App from './App/App';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-  </React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
