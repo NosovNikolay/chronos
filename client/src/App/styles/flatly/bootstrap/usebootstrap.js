700d17d2fd0dcab090cd7876ff5b0cca84d8a290
mykola <mykola@emil.de>
2022-11-26 22:45:23 +0100

    feat(client): modify styles

--- client/src/App/styles/flatly/bootstrap/usebootstrap.js
+++ client/src/App/styles/flatly/bootstrap/usebootstrap.js
@@ -1,21 +1,25 @@
-(function(){
-
-  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
-    var html = $(this).parent().html();
-    html = cleanSource(html);
-    $("#source-modal pre").text(html);
-    $("#source-modal").modal();
-  });
+(function () {
+  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(
+    function () {
+      var html = $(this).parent().html();
+      html = cleanSource(html);
+      $('#source-modal pre').text(html);
+      $('#source-modal').modal();
+    },
+  );
 
   $('.bs-component [data-toggle="popover"]').popover();
   $('.bs-component [data-toggle="tooltip"]').tooltip();
 
-  $(".bs-component").hover(function(){
-    $(this).append($button);
-    $button.show();
-  }, function(){
-    $button.hide();
-  });
+  $('.bs-component').hover(
+    function () {
+      $(this).append($button);
+      $button.show();
+    },
+    function () {
+      $button.hide();
+    },
+  );
 
   function cleanSource(html) {
     var lines = html.split(/\n/);
@@ -24,9 +28,9 @@
     lines.splice(-1, 1);
 
     var indentSize = lines[0].length - lines[0].trim().length,
-        re = new RegExp(" {" + indentSize + "}");
+      re = new RegExp(' {' + indentSize + '}');
 
-    lines = lines.map(function(line){
+    lines = lines.map(function (line) {
       if (line.match(re)) {
         line = line.substring(indentSize);
       }
@@ -34,9 +38,8 @@
       return line;
     });
 
-    lines = lines.join("\n");
+    lines = lines.join('\n');
 
     return lines;
   }
-
 })();
